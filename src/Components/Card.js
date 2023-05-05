7ac301b456fe36d377e58ad157a660e888a14eee
vikramsaharan72056 <vikramsaharan72056@gmail.com>
2023-05-05 12:14:59 +0530

    size fix

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,20 +7,27 @@ const Card = ({ id, attributes }) => {
     <div className="card" style={{ width: "150px", marginLeft: "10px" }}>
       <Link to={`/product/${id}`}>
         <div className="card-image">
-          <div>
-            <img
-              style={{ width: "100%", height: "100px" }}
-              src={attributes.Image.data[0].attributes.url}
-              alt="sample"
-            />
-            <div className="card-title black-text ">
-              <h6 style={{ margin: "-24px", fontSize: "14px" }}>
-                <b>{attributes.Name}</b>
-              </h6>
-            </div>
+          <img
+            style={{ width: "100%", height: "100px" }}
+            src={attributes.Image.data[0].attributes.url}
+            alt="sample"
+          />
+          <div className="card-title black-text ">
+            <h6
+              style={{
+                marginBottom: "-24px",
+                marginLeft: "-15px",
+                fontSize: "14px",
+              }}
+            >
+              <b>{attributes.Name}</b>
+            </h6>
           </div>
         </div>
-        <span className="card-content">
+        <span
+          className="card-content"
+          style={{ fontSize: "12px", marginTop: "-5px" }}
+        >
           <p className="truncate">{attributes.Description}</p>
           <h6 className="green-text " style={{ marginBottom: "-15px" }}>
             <b>Price:</b>&nbsp;&nbsp; {attributes.Price}
